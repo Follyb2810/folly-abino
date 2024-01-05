c863dd72ae9742bbea51155fdeaf0792d9e72c74
follyb2810 <babatundeyusuffolorunsho@gmail.com>
2024-01-04 21:03:27 -0800

    adding changes

--- index.js
+++ index.js
@@ -12,35 +12,50 @@ const folly = require('crypto').randomBytes(64).toString('hex')
 // console.log(folly)
 
 connectdb()
-app.use(
-  cors({
-    // origin: 'http://localhost:8080', 
-    origin: '*',
-    credentials: true,
-  })
-);
-app.use(cookieParser())
 
+//!chec
+
+
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
-
 app.get('/', (req, res) => {
-  res.send('Hello Folly!');
+  res.send('Hello');
 });
-app.get('/about', (req, res) => res.send('About Page Route'));
+//! chedk
+// app.use(
+//   cors({
+//     // origin: 'http://localhost:8080', 
+//     origin: '*',
+//     credentials: true,
+//   })
+// );
+// app.use(cookieParser())
 
-app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
-app.get('/contact', (req, res) => res.send('Contact Page Route'));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+// app.use(express.static('public'));
 
-app.use('/api/v1',AuthRoutes)
-app.use(protectedRoutes)
-app.use('/api/v1/refresh',RefreshUser)
-app.use('/api/v1/blog',BlogRoutes)
+
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Folly!');
+// });
+// app.get('/about', (req, res) => res.send('About Page Route'));
+
+// app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
+
+// app.get('/contact', (req, res) => res.send('Contact Page Route'));
+
+// app.use('/api/v1',AuthRoutes)
+// app.use(protectedRoutes)
+// app.use('/api/v1/refresh',RefreshUser)
+// app.use('/api/v1/blog',BlogRoutes)
 app.listen(PORT, () => console.log('Folly is listening on port ' + PORT));
 
 
